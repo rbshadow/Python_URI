6b7433fafac468e04292509b864e7d72fe854f4d
rbshadow <ridoy.computers@gmail.com>
2018-02-15 00:31:18 +0600

    Added new solutions

---
+++ Beginner/URI_1173.py
@@ -0,0 +1,9 @@
+def math():
+    j = int(input())
+    for i in range(10):
+        print('N[' + str(i) + '] =', j)
+        j = j + j
+
+
+if __name__ == '__main__':
+    math()
---
+++ Beginner/URI_1174.py
@@ -0,0 +1,10 @@
+def math():
+    for i in range(100):
+        j = float(input())
+
+        if j <= 10.0:
+            print('A[' + str(i) + '] =', j)
+
+
+if __name__ == '__main__':
+    math()
